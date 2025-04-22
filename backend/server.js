a8fc08918fc282a3f0bef124f280eb11897d61ee
Ayushsaxena0227 <saxenaayush381@gmail.com>
2025-04-22 15:28:28 +0530

    Updated: Fixed CORS and deployment issues

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,20 @@ require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mini-task-manager.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", //frontend origin
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
